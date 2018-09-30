84559e68d0b1609f505f1064bcdab00b9438e921
jerschrz <sch05044@byui.edu>
2018-09-29 22:42:27 -0600

    alignment

--- web/home.php
+++ web/home.php
@@ -39,7 +39,7 @@
 					<img src="images/babushka1.jpg" class="img-fluid float-right">
 				</div>
 				<div class="col">
-					<p class="float-left my-auto">This is Babushka<br>Babushka loves you</p>
+					<p class="float-left align-self-center">This is Babushka<br>Babushka loves you</p>
 				</div>
 			</div>
 		</div>
